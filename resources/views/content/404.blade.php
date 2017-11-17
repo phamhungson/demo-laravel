d448ebbaad1bcf6a57364e7aa1ff5c32797bdaca
phamhungson <phamsonqn97@gmail.com>
2017-11-17 15:57:47 +0700

    update theme

---
+++ resources/views/content/404.blade.php
@@ -0,0 +1,12 @@
+@extends('content/master')
+@section('content')
+<div class="single_top">
+	 <div class="container"> 
+	  <div class="error-404 text-center">
+			<h1>404</h1>
+			<p>You ' ve Failed</p>
+			<a class="b-home" href="index.html">Back to Home</a>
+		  </div>
+      </div>
+</div>
+@endsection
