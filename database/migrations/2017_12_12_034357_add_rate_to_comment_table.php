e182ac7e71ac6684db4b9e3a204a0b8a8e34e257
phamhungson <phamsonqn97@gmail.com>
2017-12-12 11:05:22 +0700

    a

---
+++ database/migrations/2017_12_12_034357_add_rate_to_comment_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddRateToCommentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comment', function (Blueprint $table) {
+            $table->mediumInteger('rate');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comment', function (Blueprint $table) {
+            $table->dropColumn('rate');
+        });
+    }
+}
